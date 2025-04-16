1ee29e24f1369b0f1c6f044624daaf4b30140e76
guih0113 <gui.henriquess13@gmail.com>
2025-04-16 13:17:51 -0300

    fixing indentation

--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -20,16 +20,16 @@ export function SecondSection() {
                     <strong className='font-semibold'>333</strong>
                     <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
                 </a>
-                    <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-                        <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-                        <strong className='font-semibold'>333</strong>
-                        <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-                    </a>
-                    <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-                        <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-                        <strong className='font-semibold'>333</strong>
-                        <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-                    </a>
+                <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                    <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                    <strong className='font-semibold'>333</strong>
+                    <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                </a>
+                <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                    <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                    <strong className='font-semibold'>333</strong>
+                    <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                </a>
             </div>
         </section>
     );
