e592da3718c106924cbd47aab515d541e4af44ca
guih0113 <gui.henriquess13@gmail.com>
2025-04-16 13:10:18 -0300

    adding components and cleaning page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { Footer } from '@/components/Footer';
 import { Sidebar } from '@/components/Sidebar';
-import { ChevronLeft, ChevronRight, Play } from 'lucide-react'
-import Image from 'next/image';
+import { NavButtons } from '@/components/NavButtons';
+import { FirstSection } from '@/components/FirstSection';
+import { SecondSection } from '@/components/SecondSection';
+import { Footer } from '@/components/Footer';
 
 export default function Home() {                                                                                                                                                                      
   return (
@@ -9,91 +10,11 @@ export default function Home() {
       <div className="flex flex-1">
         <Sidebar />  
         <main className="flex-1 p-6">
-          <div className='flex items-center gap-3'>
-            <button className='rounded-full bg-black/40 p-1'>
-              <ChevronLeft/>
-            </button>
-            <button className='rounded-full bg-black/40 p-1'>
-              <ChevronRight/>
-            </button>
-          </div>
+          <NavButtons/>
 
-          <h1 className='font-semibold text-3xl mt-10 '>Good Afternoon</h1>
+          <FirstSection/>
 
-          <div className='grid grid-cols-3 gap-4 mt-4'>
-            <a href='' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/333.jpg" width={104} height={104} alt="Capa do Álbm"/>
-              <strong>333</strong>
-              <button className='w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                <Play fill='bg-black'/>
-              </button>
-            </a>
-            <a href='' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/333.jpg" width={104} height={104} alt="Capa do Álbm"/>
-              <strong>333</strong>
-              <button className='w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                <Play fill='bg-black'/>
-              </button>
-            </a>
-            <a href='' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/333.jpg" width={104} height={104} alt="Capa do Álbm"/>
-              <strong>333</strong>
-              <button className='w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                <Play fill='bg-black'/>
-              </button>
-            </a>
-            <a href='' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/333.jpg" width={104} height={104} alt="Capa do Álbm"/>
-              <strong>333</strong>
-              <button className='w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                <Play fill='bg-black'/>
-              </button>
-            </a>
-            <a href='' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/333.jpg" width={104} height={104} alt="Capa do Álbm"/>
-              <strong>333</strong>
-              <button className='w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                <Play fill='bg-black'/>
-              </button>
-            </a>
-            <a href='' className='bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
-              <Image src="/333.jpg" width={104} height={104} alt="Capa do Álbm"/>
-              <strong>333</strong>
-              <button className='w-12 h-12 flex items-center justify-center rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
-                <Play fill='bg-black'/>
-              </button> 
-            </a>
-          </div>
-
-          <h2 className='font-semibold text-2xl mt-10 '>Made for Guilherme Henrique</h2>
-
-          <div className='grid grid-cols-8 gap-4 mt-4'>
-            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-              <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-              <strong className='font-semibold'>333</strong>
-              <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-            </a>
-            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-              <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-              <strong className='font-semibold'>333</strong>
-              <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-            </a>
-            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-              <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-              <strong className='font-semibold'>333</strong>
-              <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-            </a>
-            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-              <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-              <strong className='font-semibold'>333</strong>
-              <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-            </a>
-            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-              <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
-              <strong className='font-semibold'>333</strong>
-              <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
-            </a>
-          </div>
+          <SecondSection/>
         </main>
       </div>
 
---
+++ src/components/SecondSection.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+
+export function SecondSection() {
+    return (
+        <section>
+            <h2 className='font-semibold text-2xl mt-10 '>Made for Guilherme Henrique</h2>
+            <div className='grid grid-cols-8 gap-4 mt-4'>
+                <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                    <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                    <strong className='font-semibold'>333</strong>
+                    <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                </a>
+                <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                    <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                    <strong className='font-semibold'>333</strong>
+                    <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                </a>
+                <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                    <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                    <strong className='font-semibold'>333</strong>
+                    <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                </a>
+                    <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                        <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                        <strong className='font-semibold'>333</strong>
+                        <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                    </a>
+                    <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+                        <Image className='w-full' src="/333.jpg" width={120} height={120} alt="Capa do Álbm"/>
+                        <strong className='font-semibold'>333</strong>
+                        <span className='text-sm text-zinc-400'>Matuê, 30PRAUM</span>
+                    </a>
+            </div>
+        </section>
+    );
+}
